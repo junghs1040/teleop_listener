91575d6e20d770bfb6e235971bf6a92e7fe551ff
junghs1040 <junghs1040@naver.com>
2021-08-17 22:49:12 +0900

    third commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### This package is teleop_listener package
+
+this package subscribe the topic of telop_twist_keyboard package
+
+Required version(tested):
+- Ubuntu 18.04LTS (ROS Melodic)
+
+####How to Use 
+<pre>
+<code>
+$ rosrun teleop_listener teleop_listener
+</code>
+</pre>
+
--- src/teleop_listener.cpp
+++ src/teleop_listener.cpp
@@ -2,11 +2,21 @@
 #include "std_msgs/String.h"
 #include <geometry_msgs/Twist.h>
 
-// State Global variable
-int order = 0;
-  // stop - 0 , go saright - 1, go back - 2, go right -3, go left - 4, go up - 5, go down - 6
 
-void msgCallback(const geometry_msgs::Twist::ConstPtr& msg)
+class Test
+{
+  public:
+    Test();
+    ~Test();
+    void msgCallback(const geometry_msgs::Twist::ConstPtr& msg);
+  private:
+    // State Global variable
+    int order = 0;
+    // stop - 0 , go saright - 1, go back - 2, go right -3, go left - 4, go up - 5, go down - 6
+
+};
+
+void Test::msgCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
   float x = msg->linear.x;
   float y = msg->linear.y;
@@ -55,7 +65,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv,"teleop_listener");
   ros::NodeHandle nh;
 
-  ros::Subscriber teleop_sub = nh.subscribe("cmd_vel", 1000, msgCallback);
+  ros::Subscriber teleop_sub = nh.subscribe("cmd_vel", 1000, &Test::msgCallback, Test);
 
   ros::spin();
   return 0;
